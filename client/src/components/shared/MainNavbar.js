5659968243431ee7cf4dfc326521057242f41782
John Doe <paytonanthony2020@gmail.com>
2021-06-29 17:57:28 -0600

    added components and updated routes

--- client/src/components/shared/MainNavbar.js
+++ client/src/components/shared/MainNavbar.js
@@ -4,6 +4,8 @@ const MainNavbar = () => (
   <Navbar bg="dark" variant="dark">
     <Nav>
       <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/login">Login</Nav.Link>"
+      <Nav.Link href="/register">Register</Nav.Link>"
     </Nav>
   </Navbar>
 )
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   
-  namespace :api do
-    resources :users
-  end
 end
