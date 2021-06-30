cabdebea611f4dca7a40dbdd29c9278c442f1b63
Cameron Lund <clund6314@gmail.com>
2021-06-29 21:50:44 -0600

    added logout to the navbar after you're logged in, gave Nomatch page a link to home

--- client/src/components/shared/Nomatch.js
+++ client/src/components/shared/Nomatch.js
@@ -1,13 +1,15 @@
-import { Jumbotron } from 'react-bootstrap';
-
+import { Jumbotron, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Nomatch = () => (
-  <>
-    <Jumbotron>
-      <p>404 page</p>
-
-    </Jumbotron>
-  </>
+  <Jumbotron>
+    <h1>404 - page not found</h1>
+    <Link to="/">
+      <Button>
+        Return Home
+      </Button>
+    </Link>
+  </Jumbotron>
 )
 
 export default Nomatch;
