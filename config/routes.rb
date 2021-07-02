f5318483416f7c093016376a32e9676e9ad56eec
John Doe <paytonanthony2020@gmail.com>
2021-07-02 11:14:55 -0600

    Only finished validations and created ProtectedRoute Component

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   
-  namespace :api do
-    resources :books do
-      resources :days
-    end
-    
-    resources :days do
-      resources :cols
-    end
-
-    resources :cols do
-      resources :cards
-    end
-    
-  end 
 end
