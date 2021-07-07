3357e729e8196ca79072333d8c7459467dc7e1d8
Steph Mahnke <mahnkes1@msu.edu>
2021-07-06 18:51:44 -0600

    added days controller and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  book = Book.create(
+    title: Faker::Job.position,
+    descript: Faker::Marketing.buzzwords
+  )
+  3.times do
+    day = Day.create(
+      day_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+      book_id: book.id
+    )
+  
+    col = Col.create(
+      title: Faker::Verb.simple_present,
+      day_id: day.id
+    )
+  
+    Card.create(
+      title: Faker::Job.field,
+      end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short),
+      notes: Faker::Job.key_skill,
+      col_id: col.id
+    )
+  end
+end
+
+puts "Data seeded"
