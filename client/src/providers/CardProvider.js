aeb7a6b05ac254db07ef563370f1049b85b3c508
Leticia Fernandes <leticiatotaa@gmail.com>
2021-07-12 20:21:24 -0600

    card provider done

---
+++ client/src/providers/CardProvider.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const CardContext =  React.createContext();
+
+export const CardConsumer = CardContext.Consumer;
+
+const CardProvider = ({ children }) => {
+
+    const [cards, setCards] = useState([])
+    useEffect( () => {
+        axios.get('/api/cols/${colId}/cards')
+        .then( res => setCards(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
+   const addCard = (card) => {
+       axios.post('/api/cols/${colId}/cards', { card })
+        .then( res => {
+            setCards([...cards, res.data])
+        })
+        .catch( err => console.log(err))
+   }
+
+   const updateCard = (id, card) => {
+       axios.put(`/api/cols/${colId}/cards/${id}`, { card })
+        .then(res => {
+            const updatedCards = cards.map( c => {
+                if (c.id === id) {
+                    return res.data
+                }
+                return c
+            })
+            setCards(updatedCards)
+        })
+        .catch( err => console.log(err) )
+   }
+
+   const deleteCard = (id) => {
+       axios.delete(`/api/cols/${colId}/cards/${id}`)
+        .then( res => {
+            setCards(cards.filter(c => c.id !== id))
+            alert(res.data.message)
+        })
+        .catch( err => console.log(err) )
+
+   }
+   
+    return (
+        <CardContext.Provider value={{
+            cards, 
+            addCard: addCard,
+            updateCard: updateCard,
+            deleteCard: deleteCard,
+
+
+        }}>
+            { children }
+        </CardContext.Provider>
+    )
+}
+
+export default CardProvider;
+
