bec66108321db8cbdecfd8befed86158e38dd632
Katie Secrest <katie.secrest@gmail.com>
2021-07-12 20:58:07 -0600

    anthony

---
+++ client/src/components/books/BookColorOpts.js
@@ -0,0 +1,71 @@
+
+// import styled from 'styled-components';
+// import { Button } from 'react-bootstrap';
+
+// const BookColorOpts = () => (
+//   <>  
+//   </>
+// )
+// /* // this is how to style a package button */
+// export const MyPurpleButton = styled(Button)`
+//     border-radius: 50% 50% 49% 51% / 55% 57% 43% 45% !important;
+//     color: #9848FF !important;`
+// export const MyGreenButton = styled(Button)`
+//     border-radius: 50% 50% 49% 51% / 55% 57% 43% 45% !important;
+//     color: #407D60 !important;`
+// export const MyPurpleButton = styled(Button)`
+//     border-radius: 50% 50% 49% 51% / 55% 57% 43% 45% !important;
+//     color: #9848FF !important;`
+// export const MyPurpleButton = styled(Button)`
+//     border-radius: 50% 50% 49% 51% / 55% 57% 43% 45% !important;
+//     color: #9848FF !important;`
+// export const MyPurpleButton = styled(Button)`
+//     border-radius: 50% 50% 49% 51% / 55% 57% 43% 45% !important;
+//     color: #9848FF !important;`
+    
+    
+
+// export default BookColorOpts;
+
+
+
+
+
+
+/* .purple-btn {
+    border-radius: 20px;
+    color: #9848FF;
+}
+.green-btn {
+    border-radius: 20px;
+    color: #407D60;
+}
+.orange-btn {
+    border-radius: 20px;
+    color: #D66E33;
+}
+.aqua-btn {
+    border-radius: 20px;
+    color: #44BCE0;
+}
+.bluepurple-btn {
+    border-radius: 20px;
+    color: #42487E;
+} */
+
+
+/* // export const Colors = [
+
+//     { key: "p", text: "purple", value: "#9848FF", },
+//     { key: "g", text: "green", value: "#407D60", },
+//     { key: "o", text: "orange", value: "#D66E33", },
+//     { key: "a", text: "aqua", value: "#44BCE0", },
+//     { key: "n", text: "bluepurple", value: "#42487E", },
+// ]; */
+
+/* 
+<Button variant="purple-btn"></Button>{' '}
+<Button variant="green-btn"></Button>{' '}
+<Button variant="orange-btn"></Button>{' '}
+<Button variant="aqua-btn"></Button>{' '}
+<Button variant="bluepurple-btn"></Button>{' '}  */
