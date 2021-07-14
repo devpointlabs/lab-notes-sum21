abb02fd1b4261aaa6f7e09cbdd8296c44150f3b7
John Doe <paytonanthony2020@gmail.com>
2021-07-13 18:36:02 -0600

    got seed file working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,37 @@
-5.times do
-  book = Book.create(
-    title: Faker::Job.position,
-    descript: Faker::Marketing.buzzwords,
-
-  )
-  3.times do
-    day = Day.create(
-      day_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
-      book_id: book.id
+# 1.times do
+#   User.create(
+#     email: "tonypizza@gmail.com",
+#     password: "password",
+#     name: "user"
+#   )
+  5.times do
+    book = Book.create(
+      title: Faker::Job.position,
+      descript: Faker::Marketing.buzzwords,
+      start_date: Faker::Date.between(from: 20.days.ago, to: Date.today),
+      end_date: Faker::Date.between(from: 3.days.ago, to: Date.today),
+      color: Faker::Color.hex_color,
+      user_id: 1
     )
-  
-    col = Col.create(
-      title: Faker::Verb.simple_present,
-      day_id: day.id
-    )
-  
-    Card.create(
-      title: Faker::Job.field,
-      end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short),
-      notes: Faker::Job.key_skill,
-      col_id: col.id
-    )
-  end
+    3.times do
+      day = Day.create(
+        day_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+        book_id: book.id
+      )
+      
+      col = Col.create(
+        title: Faker::Verb.simple_present,
+        day_id: day.id
+      )
+      
+      Card.create(
+        title: Faker::Job.field,
+        notes: Faker::Job.key_skill,
+        end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short),
+        col_id: col.id
+      )
+    end
+  # end
 end
 
 puts "Data seeded"
