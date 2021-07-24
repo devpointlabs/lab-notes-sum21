c124724b7695bd4234d3323e3b5763b0cf3fe97e
Steph Mahnke <mahnkes1@msu.edu>
2021-07-24 15:55:49 -0600

    add card button on column

--- client/src/components/cards/CardForm.js
+++ client/src/components/cards/CardForm.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Form, Button, Modal, } from 'react-bootstrap';
+import { Form, Button, Modal, Image } from 'react-bootstrap';
 import { CardConsumer } from '../../providers/CardProvider';
 
 // const CardForm = ({ addCard, history }) => {
@@ -13,7 +13,6 @@ import { CardConsumer } from '../../providers/CardProvider';
   const CardForm = ({ addCard, id, title, end_time, notes, updateCard, handleEditClose, history }) => {
     const [card, setCard] = useState({ title: "", end_time: "", notes: "" })
     const [show, setShow] = useState(false);
-    
  useEffect( ()  => {
   if (id) {
     setCard({ title, end_time, notes, })
@@ -44,90 +43,51 @@ import { CardConsumer } from '../../providers/CardProvider';
 
   return (
     <>
-    <Button variant="primary" onClick={handleShow}>
-      +  New Card
-    </Button>
-    <Modal show={show} onHide={handleClose}>
-    <Modal.Header closeButton>
-      <Modal.Title>
-       Create a New Card
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicTitle">
-        <Form.Label>
-          Card Title
-        </Form.Label>
-        <Form.Control size="md"
-          name="title"
-          value={card.title}
-          onChange={(e) => setCard({ ...card, title: e.target.value })}
-          placeholder="title"
-          required
-          type="text"
-        />
-        <Form.Label>
-          Notes
-        </Form.Label>
-        <Form.Control
-          name="notes"
-          value={card.notes}
-          onChange={(e) => setCard({ ...card, notes: e.target.value })}
-          placeholder="notes"
-          required
-          as="textarea" rows={3}
-        />
-        <Form.Label>
-          What Time?
-        </Form.Label>
-        <Form.Control
-          name="end_time"
-          value={card.end_time}
-          onChange={(e) => setCard({ ...card, end_time: e.target.value })}
-          placeholder="end_time"
-          required
-          type= "time"
-        />
-        </Form.Group>
-      <Button variant="primary" onClick={refreshpage} type='submit'>
-        Submit
+      <Button variant="light" onClick={handleShow}>
+        <Image className="add" 
+        src="https://res.cloudinary.com/dg1eqxvwf/image/upload/v1627152011/Screen_Shot_2021-07-24_at_12.38.51_PM_jc5dfm.png"
+        roundedCircle
+        align='right'
+        width="30"
+        height="30" 
+        alt="add"/>
       </Button>
-      </Form>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="secondary" onClick={handleClose}>
-        Close
-      </Button>
-    </Modal.Footer>
-  </Modal>
-    {/* <Form onSubmit={handleSubmit}>
-      <input
-        name="title"
-        value={card.title}
-        onChange={(e) => setCard({ ...card, title: e.target.value })}
-        placeholder="title"
-        required
-      />
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Create a New Card
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+            <input
+              name="title"
+              value={card.title}
+              onChange={(e) => setCard({ ...card, title: e.target.value })}
+              placeholder="title"
+              required
+            />
 
-      <input
-        name="end_time"
-        value={card.end_time}
-        onChange={(e) => setCard({ ...card, end_time: e.target.value })}
-        placeholder="end_time"
-        required
-      />
+            <input
+              name="end_time"
+              value={card.end_time}
+              onChange={(e) => setCard({ ...card, end_time: e.target.value })}
+              placeholder="end_time"
+              required
+            />
 
-      <input
-        name="notes"
-        value={card.notes}
-        onChange={(e) => setCard({ ...card, notes: e.target.value })}
-        placeholder="notes"
-        required
-      />
-      <button type="submit">Submit</button>
-    </Form> */}
-    </>
+            <input
+              name="notes"
+              value={card.notes}
+              onChange={(e) => setCard({ ...card, notes: e.target.value })}
+              placeholder="notes"
+              required
+            />
+          <button type="submit">Submit</button>
+        </form>
+      </Modal.Body>
+    </Modal>
+  </>
   )
 }
 
