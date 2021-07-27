b8c768b1457927bb7d2bd4cf5be3e67957c71417
Cameron Lund <clund6314@gmail.com>
2021-07-27 17:40:24 -0600

    cleaned up code

--- client/src/components/books/BookShow.js
+++ client/src/components/books/BookShow.js
@@ -2,34 +2,20 @@ import { Button, Modal, Card } from 'react-bootstrap';
 import { useState } from 'react';
 import BookEdit from './BookEdit';
 import { BookConsumer } from '../../providers/BookProvider';
-import Moment from 'react-moment'; 
 import DayList from '../days/DayList';
 import {CenterBookShow} from '../styledComponents/BookShowStyle';
 
 const BookShow = ({ location, deleteBook, match, history }) => {
-  // const bookContext = useContext(BookContext);
   const [editshow, setEditShow] = useState(false);
   const handleEditClose = () => setEditShow(false);
   const handleEditShow = () => setEditShow(true);
   return (
     <>
     <CenterBookShow>
-      {/* <h1>Book Show # {location.state.id}</h1>
-      <h1>Checkout Show # {match.params.id}</h1> */}
       <Card style={{width: '18rem'}} className="text-center">
       <Card.Header >
       <text style={{fontWeight: "bold"}}>{location.state.title}</text>
       </Card.Header>
-      {/* <p>
-        
-        Book Description: {location.state.descript}
-      </p>
-      <p> 
-        Tracking Begin Date: <Moment format="MM-DD-YYYY">{location.state.start_date}</Moment>
-      </p>
-      <p>
-        Tracking End Date: <Moment format="MM-DD-YYYY">{location.state.end_date}</Moment>
-      </p> */}
       <Card.Body>
         <DayList bookId={location.state.id}/>
       </Card.Body>
