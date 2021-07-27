f72f329fb7b2d3496622e271b9c9c30fdf663bb3
John Doe <paytonanthony2020@gmail.com>
2021-07-26 20:35:36 -0600

    finished all crud actions for the cards

--- client/src/components/cards/CardShow.js
+++ client/src/components/cards/CardShow.js
@@ -1,12 +1,14 @@
 import { Card, Button, Modal, } from 'react-bootstrap';
 import CardForm from './CardForm';
+import { useState } from 'react';
 
 const CardShow = ({ ColId,  deleteCard, history, id, title, notes, end_time, updateCard, }) => {
+  const [show, setShow] = useState(false)
 
   return (
     <>
       <Card style={{ width: '18rem' }} key={id}>
-        <Card.Body>
+        <Card.Body onClick={() => setShow(true)}>
           <Card.Title>{title}</Card.Title>
           <Card.Text>
             {notes}
@@ -28,6 +30,16 @@ const CardShow = ({ ColId,  deleteCard, history, id, title, notes, end_time, upd
           updateCard={updateCard}
         />
       </Card>
+      <Modal show={show} fullscreen={true} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {title} {end_time}
+          </Modal.Title>
+          <Modal.Body>
+            {notes}
+          </Modal.Body>
+        </Modal.Header>
+      </Modal>
     </>
   )
 }
