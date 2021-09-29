2c73d3c71e871dbb97e3ffdbcbb25e268bbfde11
Anthony Payton <84427236+anthony-patyton@users.noreply.github.com>
2021-09-28 20:40:39 -0600

    updated layout static_controller

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,7 +1,8 @@
-require "test_helper"
+require 'rails/application_controller'
+class StaticController < Rails::ApplicationController
+  #  layout false
 
-class StaticControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+   def index
+     render file: Rails.root.join('public', 'index.html')
+   end
 end
